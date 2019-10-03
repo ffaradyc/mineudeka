4b6ec8c97f1b618d6da24b55f48a9c966a7be434
Funny Farady Coastera <ffaradyc@unib.ac.id>
2019-10-04 03:04:05 +0700

    Ingredients

--- lib/main/ingredients.dart
+++ lib/main/ingredients.dart
@@ -18,8 +18,7 @@ class Ingredients extends StatelessWidget {
             height: 30.0,
           ),
           Container(
-            margin: EdgeInsets.symmetric(vertical: 20.0),
-            height:280.0,
+            height: 200.0,
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
@@ -33,12 +32,15 @@ class Ingredients extends StatelessWidget {
               ],
             ),
           ),
+          SizedBox(
+            height: 20.0,
+          ),
           Center(
             child: Text(
               'Mineudeka!',
               style: TextStyle(
                 fontFamily: 'Nunito',
-                fontSize: 56.0,
+                fontSize: 36.0,
                 fontWeight: FontWeight.w900,
                 color: Color(0xff710099),
                 shadows: [
@@ -53,22 +55,29 @@ class Ingredients extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.purple,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.white),
-            title: Text('Home', style: TextStyle(color: Colors.white)),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.android, color: Colors.white70),
-            title: Text('Home', style: TextStyle(color: Colors.white70)),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo, color: Colors.white70),
-            title: Text('Home', style: TextStyle(color: Colors.white70)),
-          ),
-        ],
+      bottomNavigationBar: SizedBox(
+        height: 48.0,
+        child: BottomNavigationBar(
+          iconSize: 20,
+          elevation: 10.0,
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          backgroundColor: Colors.purple,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.white),
+              title: Text('Home', style: TextStyle(color: Colors.white)),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.android, color: Colors.white70),
+              title: Text('Home', style: TextStyle(color: Colors.white70)),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.photo, color: Colors.white70),
+              title: Text('Home', style: TextStyle(color: Colors.white70)),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -80,9 +89,9 @@ class IngredientsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(left: 20.0, right: 20.0),
-      height: 280.0,
-      width: 180.0,
+      margin: EdgeInsets.only(left: 5.0, right: 5.0),
+      height: 200.0,
+      width: 130.0,
       padding: EdgeInsets.all(10.0),
       alignment: Alignment.center,
       decoration: BoxDecoration(
@@ -94,30 +103,33 @@ class IngredientsList extends StatelessWidget {
           Radius.circular(25.0),
         ),
       ),
-      child: Column(
-        children: <Widget>[
-          Center(
-            child: Text(
-              'Orange Mojito',
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Orange Mojito',
+                style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white60,
+                  height: 1.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text(
+              'Aute culpa esse adipisicing mollit qui eiusmod. Amet tempor consectetur pariatur tempor aute magna id officia commodo aute ipsum. Irure consequat consectetur incididunt magna. Lorem do quis sunt nostrud culpa laboris non et aliquip.',
               style: TextStyle(
                 fontFamily: 'Nunito',
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white60,
+                fontSize: 8.0,
+                color: Colors.white,
               ),
-              textAlign: TextAlign.center,
+              textAlign: TextAlign.justify,
             ),
-          ),
-          Text(
-            'Aute culpa esse adipisicing mollit qui eiusmod. Amet tempor consectetur pariatur tempor aute magna id officia commodo aute ipsum. Irure consequat consectetur incididunt magna. Lorem do quis sunt nostrud culpa laboris non et aliquip.',
-            style: TextStyle(
-              fontFamily: 'Nunito',
-              fontSize: 12.0,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.justify,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
