ea04dde9701f8077903f1c09a0eb6524aa57225f
FirmanJabar <firmanabduljabar@gmail.com>
2019-10-05 18:10:10 +0800

    menu-about

---
+++ lib/model/author.dart
@@ -0,0 +1,25 @@
+class Author {
+  String name;
+  String about;
+  String photos;
+
+  Author({this.name, this.about, this.photos});
+}
+
+final authorList = [
+  Author(
+      name: "Eudeka-OSG05",
+      photos: 'https://www.dicoding.com/images/original/event/gits_class_14_fundamental_net_core_logo_100419101235.png',
+      about:
+      "Salah satu program dari Eudeka! untuk belajar secara full online via Whatsapp dan Google Classroom, dengan para praktisi handal di bidangnya."),
+  Author(
+      name: "Firman Abdul Jabar",
+      photos: 'https://scontent-sin2-2.xx.fbcdn.net/v/t1.0-9/32828411_1968158346551983_3339780626797559808_n.jpg?_nc_cat=103&_nc_oc=AQmPBGlI8RUN7MFeZBZGvjKEEephREXZJxOOwNeRFRi6HQDTrWTUnsiFlRY_QbwajjU&_nc_ht=scontent-sin2-2.xx&oh=21977f294c6b2272899cd4336829b23b&oe=5E300E98',
+      about:
+      "All the impossible is possible for those who believe!"),
+  Author(
+      name: "Funny Farady Coastera",
+      photos: 'https://avatars0.githubusercontent.com/u/24467531?s=400&v=4',
+      about:
+      "Be yourself; cause everybody is taken."),
+];
